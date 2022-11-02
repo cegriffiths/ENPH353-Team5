abb35b0d3fa65b9c19d8eaa4ad1f30fc733f24f0
Chris Griffiths <cegriffiths9@gmail.com>
2022-11-02 16:47:35 -0700

    Adding all pre-existing ROS files

---
+++ ros_ws/devel/share/gennodejs/ros/enph353_gazebo/srv/_index.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+let SubmitPlate = require('./SubmitPlate.js')
+let GetLegalPlates = require('./GetLegalPlates.js')
+
+module.exports = {
+  SubmitPlate: SubmitPlate,
+  GetLegalPlates: GetLegalPlates,
+};
